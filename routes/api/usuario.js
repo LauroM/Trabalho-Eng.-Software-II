38f12816107a333379afb1ea089fb3b19f49173f
Yuri Braga <yuri@Yuri-PC.localdomain>
2019-10-17 09:25:13 -0300

    User register

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -1,8 +1,35 @@
-    
 const router = require("express").Router();
+const Usuario = require("../../db/models").Usuario;
 
-router.post("/cadastro", (req, res) => {
-  res.status(200).send({ msg: "Usuario route works" });
+/**
+ * Register a new user
+ */
+router.post("/register", (req, res) => {
+  let userFields = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  Usuario.findOrCreate({
+    where: {
+      username: userFields.username
+    },
+    defaults: userFields
+  })
+    .then(([usuario, created]) => {
+      if (!created) {
+        return res.status(400).send({
+          error: {
+            userExists: "Usuário já existe na base de dados"
+          }
+        });
+      }
+
+      return res.status(200).send({ success: true, usuario });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
